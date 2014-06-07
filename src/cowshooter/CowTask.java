89a8485e0ec87b6a411759020502e95036f03641
Hao <wei-da.hao@kfwh000.edu>
2014-06-07 18:53:02 -0500

    Intentionally make mistakes!

--- src/cowshooter/CowTask.java
+++ src/cowshooter/CowTask.java
@@ -6,6 +6,8 @@
  * We make no guarantees that this code is fit for any purpose. 
  * Visit http://www.pragmaticprogrammer.com/titles/ahmine for more book information.
 ***/
+
+Zombie cow is coming. Run Braaaaaaaaaaaaaaaa
 package cowshooter;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
@@ -13,7 +15,7 @@ import org.bukkit.entity.Cow;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-public class CowTask extends BukkitRunnable {
+
     private World world;
     private Cow cow;
     public CowTask(World myWorld, Cow myCow) {
