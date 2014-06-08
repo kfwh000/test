7a31ba91cd6f0bb79b708407a8ca3620b2f06aba
Hao <wei-da.hao@kfwh000.edu>
2014-06-07 19:04:09 -0500

    all files

--- src/cowshooter/CowTask.java
+++ src/cowshooter/CowTask.java
@@ -7,7 +7,8 @@
  * Visit http://www.pragmaticprogrammer.com/titles/ahmine for more book information.
 ***/
 
-Zombie cow is coming. Run Braaaaaaaaaaaaaaaa
+//Zombie cow is coming. Run Braaaaaaaaaaaaaaaa
+
 package cowshooter;
 import org.bukkit.Bukkit;
 import org.bukkit.World;
@@ -15,7 +16,7 @@ import org.bukkit.entity.Cow;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.scheduler.BukkitRunnable;
-
+public class CowTask extends BukkitRunnable {
     private World world;
     private Cow cow;
     public CowTask(World myWorld, Cow myCow) {
